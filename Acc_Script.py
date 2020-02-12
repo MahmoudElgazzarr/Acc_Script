ebfd34c69b496d75f5dbddc77115eaa99426c925
Mahmoud Elgazzar <Mahmoud.Elgazzar@AVELABS.CAI>
2020-02-13 00:17:01 +0200

    Added Saving the application datat to coulmun 5

--- Acc_Script.py
+++ Acc_Script.py
@@ -22,7 +22,9 @@ sheet_obj = wb_obj.get_sheet_by_name('1D_Tables')
 #get max numbers of rows
 maxmium_row = sheet_obj.max_row
 
-DataTypes_Found_At_Lines = []
+#declare a list to save lines number in it , initalize by zeros
+DataTypes_Found_At_Lines = [0] * maxmium_row
+
 #The Implementation Datatype we are searching for start and end lines
 Line_Start = '<IMPLEMENTATION-DATA-TYPE-REF'
 Line_End = '</IMPLEMENTATION-DATA-TYPE-REF>'
@@ -45,8 +47,7 @@ for i in range(1, maxmium_row + 1 ):
                 #found the Implementation data type But flag = 1
                 Found_Datatype_Flag = 1
                 #Save Line numbers of the found Elements
-                DataTypes_Found_At_Lines.append (num_line)
-                #Get Application Data Type TODO
+                DataTypes_Found_At_Lines.insert (i,num_line)
                 break
 
             #couldn't found the calibartion implementation data set    
@@ -55,12 +56,20 @@ for i in range(1, maxmium_row + 1 ):
                 
         if(Found_Datatype_Flag != 1):
             print("Couldn't find : "+ str(cell_obj.value))
-        
+
+#Get Application Data Type
 for i in range(1, maxmium_row + 1 ):
     with open(arxml_DataTypes,'r') as inFile:
+        #Get Line numbers - 1
         for num_line, line_content in enumerate(inFile, 1):
-            if num_line == DataTypes_Found_At_Lines[i] - 1:
+            if num_line == DataTypes_Found_At_Lines[i] - 1 and DataTypes_Found_At_Lines[i] != 0 :
+                #print Application datatype
                 print(line_content)
+                #Save to the Excel Sheet
+                sheet_obj.cell(row = i, column = 5).value = line_content
+                wb_obj.save(excel_DataTypes)
+
+
     
 
 print("Hi It is Working")
