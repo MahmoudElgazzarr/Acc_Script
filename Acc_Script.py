882d6e40f41ff304fc1fce43bb9e824ddcbe2eac
m7moudelgazzar <m7moudelgazzar@gmail.com>
2020-03-07 03:47:52 +0200

    Changed Strategy , one click for all SWCs , SWC must be at column 4 , it must match the name in filepath
    Signed-off-by: m7moudelgaz

--- Acc_Script.py
+++ Acc_Script.py
@@ -8,15 +8,16 @@
 #import libraries being used
 import os
 import openpyxl
+from openpyxl.styles import PatternFill
 
 #Working
 #SW version 2.5
 
 #add folder that contain .arxml file 
-arxml_DataTypes =  "D:\DataTypes.arxml"
+arxml_DataTypes =  "C:\DataTypes.arxml"
 
 #add path that contain .xlsx file which have data type
-excel_DataTypes =  "D:/table_info_Data_Stage_B_PATH_3.xlsx"
+excel_DataTypes =  "C:/table_info_Data_Stage_B_PATH_3.xlsx"
 
 # Create temp file to copy the arxmls, and edit in the new file 
 #data_index = arxml_DataTypes.find("\\")
@@ -26,7 +27,7 @@ excel_DataTypes =  "D:/table_info_Data_Stage_B_PATH_3.xlsx"
 wb_obj = openpyxl.load_workbook(excel_DataTypes)
 
 #create Sheet object
-sheet_obj = wb_obj.get_sheet_by_name('2D_Tables')
+sheet_obj = wb_obj.get_sheet_by_name('1D_Tables')
 
 #get max numbers of rows
 maxmium_row = sheet_obj.max_row
@@ -43,13 +44,17 @@ Line_End = '</IMPLEMENTATION-DATA-TYPE-REF>'
 DATA_TYPE_MAP_tag = 0
 Current_line_where_is_tag = 0
 
+pinkfill = PatternFill(fill_type=None,start_color='F2DCDB',end_color='F2DCDB')
+sheet_obj['A1'].fill = pinkfill
+sheet_obj['A2'].fill = pinkfill
+sheet_obj['A3'].fill = pinkfill
+
 #Step 1
 #For Loop For The two Columns
-for j in range(1,4):
+for j in range(1,3):
     #for loop for the max number of row in the sheet , serach one by one
     for i in range(1, maxmium_row + 1 ):
         with open(arxml_DataTypes,'r') as inFile:
-            
             #Get Cell object Data
             cell_obj = sheet_obj.cell(row = i, column = j)
             #search in the whole file , Sequencal search Slow $need to be updated to binary search for example
--- Acc_Script_Rename.py
+++ Acc_Script_Rename.py
@@ -1,36 +1,21 @@
 #This script is being used to Rename Implementation data type to Application data Type
 
-#Testing Stach
-
 #import libraries being used
 import os
 import openpyxl
-
+import glob
 
 #add folder that contain .arxml file 
-Input_arxml =  "D:/Ford_Dat2.1/aptiv_sw/autosar_cfg/davinci/Config/Developer/ComponentTypes\\CtAp_ACC.arxml"
+SWC_Arxmls = "D:/Avelabs/Ford_Dat2.1/aptiv_sw/autosar_cfg/davinci/Config/Developer/ComponentTypes/*.arxml"
+
+#Get All Paths for Arxmls in the folder
+SWC_Arxmls_Path = glob.glob(SWC_Arxmls)
 
 #add path that contain .xlsx file which have data type
 excel_DataTypes =  "D:/table_info_Data_Stage_B_PATH_3.xlsx"
 
-# Create temp file to copy the arxmls, and edit in the new file 
-data_index = Input_arxml.find("\\")
-new_Input_arxml = Input_arxml[:(data_index)] +"/new_"+Input_arxml[(data_index+1):]
-
-# workbook object is created 
-wb_obj = openpyxl.load_workbook(excel_DataTypes)
-
-#create Sheet object
-sheet_obj = wb_obj.get_sheet_by_name('2D_Tables')
-
-#get max numbers of rows
-#maxmium_row = sheet_obj.max_row
-
 #Start Row
 Start_Row = 2
-
-#Maxmium Row + 1
-maxmium_row = 25
 
 #Line Start
 Line_Start = '<TYPE-TREF DEST="IMPLEMENTATION-DATA-TYPE">'
@@ -39,104 +24,115 @@ Middle_Line = 'Cal_Datatype'
 #Line End
 Line_End = '</TYPE-TREF>'
 
-#Check for number of changes
-Number_Of_Changes = 0
-Non_Null_Cells = 0
-Number_Of_Found_Calibration = 0
-Number_Of_None = 0
 Idt_Found = 0
-i = 0
-j = 0
-
-#Changed Lines Numbers
-Changed_list_Lines = [0] * maxmium_row
+#Global variable to hold column number
+Columns = 0
+#Global Variable to hold row number
+rows = 0
 
 flag = 1
 
+def main():
+    # workbook object is created , create Sheet object
+    Sheet_object = Load_Sheet(excel_DataTypes , '1D_Tables')
+    #get max numbers of rows
+    maxmium_row = Get_Max_Row(Sheet_object) + 1
+    #Todo Change to 4 in case of 3D Cals
+    for Column in range(1 , 3 ):
+        for row in range(Start_Row , maxmium_row ) :
+            #get Cal Parameters from excel sheet
+            Cal_Name = Get_Value_Of_Cell(Sheet_object , row , Column )
+            #get Implementation Parameters from excel sheet
+            Imp_DataType = Get_Value_Of_Cell(Sheet_object , row , Column + 7 )
+            #get application Type
+            Application_Type = Get_Value_Of_Cell(Sheet_object , row , Column + 4 )
+            #get SWC Name
+            SWC_Name = Get_Value_Of_Cell(Sheet_object , row ,  4 )
+            #Loop For all Arxmls
+            for SWC_arxml in SWC_Arxmls_Path:
+                #Search For SWC Needed
+                if SWC_arxml.find(SWC_Name) != -1:
+                    # Create temp file to copy the arxmls, and edit in the new file
+                    data_index = SWC_arxml.find("\\")
+                    new_SWC_arxml = SWC_arxml[:(data_index)] +"/new_"+SWC_arxml[(data_index+1):]
+                    #open the new file
+                    new_file = open(new_SWC_arxml,'w')
+                    #open the old file
+                    with open(SWC_arxml,'r') as inFile:
+                        #Search in the whole file
+                        for num_line, line_content in enumerate(inFile, 1):
+                            #if found the the Cal Name
+                            if line_content.find('<SHORT-NAME>'+Cal_Name+'</SHORT-NAME>') != -1:
+                                Cal_Name_Found_Flag  = 1
+                            #Replace
+                            if (line_content.find(Imp_DataType) != -1) and (Cal_Name_Found_Flag == 1) :
+                                if (Application_Type != 'None'):
+                                    #rename implementation to Application
+                                    line_content = line_content.replace(Imp_DataType, Application_Type)
+                                    #Change other headers
+                                    line_content = line_content.replace('"IMPLEMENTATION-DATA-TYPE">' , '"APPLICATION-PRIMITIVE-DATA-TYPE">')
+                                    Component_Start_in_line = line_content.find('ComponentType/')
+                                    Component_End_in_line = line_content.find('Cal_Datatype/')
+                                    line_content = line_content.replace(line_content[Component_Start_in_line : Component_End_in_line + 13 ],'Data_Type/Application_Types/')
+                                    #Write Done To Excel Sheet
+                                    Write_Value_To_Cell(Sheet_object , row , Column + 15 , 'Done' )
+                                    #Write To new file only changed iterms
+                                    #new_file.write(line_content)
+                                    #Print Line in Console
+                                    print(line_content)
+                            #Found Tag Closing
+                            if line_content.find('</PARAMETER-DATA-PROTOTYPE>') != -1:
+                                Cal_Name_Found_Flag = 0
+                            #Write Line
+                            new_file.write(line_content)
+                    #Close the File after Edits
+                    new_file.close()
+                    Copy_File_Content(new_SWC_arxml , SWC_arxml )
+
+                    # remove the orignal file to replace the new one with it   
+                    #os.remove(SWC_arxml)
+                    # rename the new file to have the same name of the orignal one 
+                    #os.rename(new_SWC_arxml,SWC_arxml)
+    #Done
+    print("Renaming Completed Successfully")
 
 
-#Search through the Excel Sheet
-#Two Columns
-for Coulumns in range(1,4):
-    #Search in the Rows
-    for rows in range(Start_Row , maxmium_row):
-        #Get value of cells
-        Application_Cell = sheet_obj.cell(row = rows, column = Coulumns + 4).value
-        #If we find none increment the variable
-        if Application_Cell is None :
-            #increment the number of None Cell
-            Number_Of_None = Number_Of_None + 1
 
-new_file = open(new_Input_arxml,'w')
-with open(Input_arxml,'r') as inFile:
-    #search in the whole file , Sequencal search Slow $need to be updated to binary search for example
-    for num_line, line_content in enumerate(inFile, 1):
-        #Copy ACC File Line by Line
-        for j in range(1,4):
-            #for loop for the max number of row in the sheet , serach one by one
-            for i in range(Start_Row , maxmium_row):
-                #Get Cell object Data
-                cell_obj = sheet_obj.cell(row = i, column = j + 7 )
-                #get Implementation Parameters from excel sheet
-                Imp_DataType = str(cell_obj.value)
-                #get Cal Parameters from excel sheet
-                Cal_obj = sheet_obj.cell(row = i, column = j )
-                #get Cal Name
-                Cal_Name = str(Cal_obj.value)
-                #search for Implementation cal line
-                if line_content.find('<SHORT-NAME>'+Cal_Name+'</SHORT-NAME>') != -1:
-                    Cal_Name_Found_Flag  = 1
-                    #print (line_content)
-                    #Number of Found Calibration
-                    Number_Of_Found_Calibration = Number_Of_Found_Calibration + 1
-                #replace
-                if  (line_content.find(Imp_DataType) != -1) and (Cal_Name_Found_Flag == 1) :
-                    #Found Cal and Idt In lines
-                    Idt_Found = Idt_Found + 1
-                    #get application Type
-                    Application_Type = str(sheet_obj.cell(row = i, column = j + 4).value)
-                    #replace line contenet with the new line must be not none
-                    if (Application_Type != 'None'):
-                        print(i,j)
-                        #number of Non NULL Cells
-                        Non_Null_Cells = Non_Null_Cells + 1
-                        #rename implementation to Application
-                        line_content = line_content.replace(Imp_DataType, Application_Type)
-                        #Change other headers
-                        line_content = line_content.replace('"IMPLEMENTATION-DATA-TYPE">' , '"APPLICATION-PRIMITIVE-DATA-TYPE">')
-                        Component_Start_in_line = line_content.find('ComponentType/')
-                        Component_End_in_line = line_content.find('Cal_Datatype/')
-                        line_content = line_content.replace(line_content[Component_Start_in_line : Component_End_in_line + 13 ],'Data_Type/Application_Types/')
-                        #print Final line
-                        #print(line_content)
-                        Number_Of_Changes = Number_Of_Changes + 1
-                if line_content.find('</PARAMETER-DATA-PROTOTYPE>') != -1:
-                    Cal_Name_Found_Flag = 0
-        # Write the line after edits "if needed" in new file
-        new_file.write(line_content)
-    #print Number of cells in the excel sheet
-    Cells_Count = maxmium_row - Start_Row
-    #Two Columns
-    Cells_Count = Cells_Count * 2
-    #Print Number of Cells
-    print('Number of Cells = ', Cells_Count)
-    #print Number of found Cal
-    print('Number_Of_Found_Calibration',Number_Of_Found_Calibration)
-    #Print number of None
-    print('Print Number of None : ',Number_Of_None)
-    #print number of IDT found
-    print('Idt_Found = ', Idt_Found)
-    #Print Number on Non Null Cells
-    print('Non Null Cells = ' , Non_Null_Cells)
-    #print Number of changes in the file
-    print('Number Of Changes = ' , Number_Of_Changes)
-    #close and save temp file         
-    new_file.close()
 
-# remove the orignal file to replace the new one with it   
-os.remove(Input_arxml)
-# rename the new file to have the same name of the orignal one 
-os.rename(new_Input_arxml,Input_arxml)
 
-#Done
-print("Renaming Completed Successfully")
+
+
+#Function that takes input path , sheet name , and create a object from the sheet
+def Load_Sheet(Input_Path , Sheet_Name):
+    wb_obj = openpyxl.load_workbook(Input_Path)
+    Sheet_Object = wb_obj.get_sheet_by_name(Sheet_Name)
+    return Sheet_Object
+
+#function that takes a sheet object and return max row
+def Get_Max_Row(sheet_obj):
+    Max = sheet_obj.max_row
+    return Max
+
+#Function that takes sheet object , ROW , Coulumn
+#Return value of a cell
+def Get_Value_Of_Cell(sheet_obj , row , column):
+    Cell_objj = sheet_obj.cell(row = row , column = column)
+    Cell_Value = str(Cell_objj.value)
+    return Cell_Value
+
+#write value to any cell in the excel sheet
+def Write_Value_To_Cell(sheet_obj , row , column , value):
+    Cell_obj = sheet_obj.cell(row = row , column = column)
+    Cell_obj.value = value
+
+#Function that copies All contenet of a file and copies to an other file
+def Copy_File_Content(src , dest):
+    with open(src,'r') as F1:
+        with open(dest,'w') as F2:
+            for Line in F1:
+                F2.write(Line)
+    F1.close()
+    F2.close()
+
+if __name__ == '__main__':
+    main()
