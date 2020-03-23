fa590046fbfc17c4f0fabe109033761c2026c20f
Mahmoud Elgazzar <Mahmoud.Elgazzar@avelabs.com>
2020-03-23 13:51:27 +0200

    Adding datatype map
    Adding datatype map removing and copying file

--- Remove_Adt_Script.py
+++ Remove_Adt_Script.py
@@ -26,6 +26,9 @@ Start_Row = 1
 #Flag for DATA_TYPE_MAP_Line_Start_found
 DATA_TYPE_MAP_Line_Start_found = 0
 
+new_Datatypes_arxml = 'D:/new_DataTypes.arxml'
+
+
 #The APPLICATION Datatype we are searching for start and end lines
 APPLICATION_Line_Start = '<APPLICATION-PRIMITIVE-DATA-TYPE'
 APPLICATION_Line_End = '</APPLICATION-PRIMITIVE-DATA-TYPE>'
@@ -36,6 +39,8 @@ DATA_TYPE_MAP_Line_End = '</DATA-TYPE-MAP>'
 def main():
     for Column in range(1 , Column_Max_Number ):
         for row in range(Start_Row , maxmium_row ) :
+            #open the new file
+            new_file = open(new_Datatypes_arxml,'w')
             #get Cal Parameters from excel sheet
             Cal_Name = Get_Value_Of_Cell(Sheet_object , row , Column )
             with open(arxml_DataTypes,'r') as inFile:
@@ -45,12 +50,21 @@ def main():
                     if line_content.find(DATA_TYPE_MAP_Line_Start) != -1 :
                         #if found set a flage 
                         DATA_TYPE_MAP_Line_Start_found = 1
-
-                    if((line_content.find(Cal_Name) != -1) and (DATA_TYPE_MAP_Line_Start_found == 1)):
-                        print('found it')
-
-
-
+                    #if cal found
+                    if((line_content.find(Cal_Name) != -1) and (DATA_TYPE_MAP_Line_Start_found == 1) and (line_content.find('<APPLICATION-DATA-TYPE-REF') != -1 )):
+                        #Remove Adt_ and also _T From the Line
+                        line_content.replace('Adt_' , '')
+                        line_content.replace('_T' , '')
+                        print("Removing")
+                    #Remove Flage for closing flag
+                    if line_content.find(DATA_TYPE_MAP_Line_End) != -1:
+                        DATA_TYPE_MAP_Line_Start_found = 0
+                    #Write Line
+                    new_file.write(line_content)
+            #Close the File after Edits
+            new_file.close()
+            #Copt New File to the old file every Line Change
+            Copy_File_Content(new_Datatypes_arxml , arxml_DataTypes )
 
 #Function that takes sheet object , ROW , Coulumn
 #Return value of a cell
@@ -58,6 +72,15 @@ def Get_Value_Of_Cell(sheet_obj , row , column):
     Cell_objj = sheet_obj.cell(row = row , column = column)
     Cell_Value = str(Cell_objj.value)
     return Cell_Value
+
+#Function that copies All contenet of a file and copies to an other file
+def Copy_File_Content(src , dest):
+    with open(src,'r') as F1:
+        with open(dest,'w') as F2:
+            for Line in F1:
+                F2.write(Line)
+    F1.close()
+    F2.close()
 
 #Start App
 if __name__ == '__main__':
